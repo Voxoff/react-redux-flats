b46a593ace8455c10a2e31bce795a9b545982f41
Voxoff <guybennettjones@hotmail.co.uk>
2018-08-11 13:21:28 +0100

    added markers to map from address

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -2,20 +2,41 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { connect } from 'react-redux';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
 
 class SimpleMap extends Component {
   render() {
-    console.log(this.props.selectedFlat)
-    let marker = null;
-    let center = { lat: this.props.selectedFlat.lat, lng: this.props.selectedFlat.lng };
+    let markers = this.props.flats.map((flat) => {return {lat: flat.lat, lng: flat.lng, name: flat.name}});
+    console.log(markers)
+    const Marker = ({ text, lat, lng }) => (
+      <div style={{
+        color: 'white', 
+        background: 'grey',
+        padding: '15px 10px',
+        display: 'inline-flex',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '100%',
+        transform: 'translate(-50%, -50%)'
+      }}>
+        {text}
+      </div>
+    );
+
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
-          center={center}
+          center={{ lat: this.props.selectedFlat.lat, lng: this.props.selectedFlat.lng }}
           defaultZoom={15}
         >
+        {console.log(this.props.flats[0])}
+        {this.props.flats.map(function(flat) { return <Marker lat={flat.lat} 
+          lng={flat.lng} 
+          text={flat.name} 
+          key={flat.name}/>
+        })}
         </GoogleMapReact>
       </div>
     );
@@ -24,7 +45,8 @@ class SimpleMap extends Component {
 
 function mapStateToProps(state) {
   return{
-    selectedFlat: state.selectedFlat 
+    selectedFlat: state.selectedFlat,
+    flats: state.flats 
   }
 }
 
